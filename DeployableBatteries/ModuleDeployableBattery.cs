999f4d380741950e4639a6e9a25b0e8354c6eb7f
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-11-03 13:58:21 -0500

    Initial commit

---
+++ DeployableBatteries/ModuleDeployableBattery.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using UnityEngine;
+using Experience.Effects;
+using KSP.Localization;
+using KSP_Log;
+
+namespace DeployableBatteries
+{
+    static class BaseFieldExtensions
+    {
+        static public UI_Control uiControlCurrent(this BaseField field)
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                return field.uiControlFlight;
+            }
+            else if (HighLogic.LoadedSceneIsEditor)
+            {
+                return field.uiControlEditor;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public class ModuleDeployableBattery : ModuleGroundSciencePart, IModuleInfo
+    {
+
+        [KSPField(isPersistant = false, guiActive = true, guiActiveUnfocused = true, guiName = "Electric Charge", guiFormat = "F2"),
+        UI_ProgressBar(minValue = 0f, maxValue = 400f, scene = UI_Scene.Flight)]
+        public float currentEC = 0;
+
+        [KSPField(isPersistant = false, guiActive = true, guiActiveUnfocused = true, guiName = "Power-Unit-Hours", guiFormat = "F2")]
+        public float powerUnitHours = 0;
+
+        [KSPField(isPersistant = true, guiActive = true, guiActiveUnfocused = true, guiName = "Max Power Unit Flow"),
+            UI_FloatRange(stepIncrement = 1f, maxValue = 5f, minValue = 0f)]
+        public float maxPowerUnitsFlow = 1;
+
+        [KSPField(isPersistant = true)]
+        public float kerbalEffectAdjustments = 1;
+
+        [KSPField]
+        public float solarPanelChargeRate = 0.35f;
+
+        [KSPField]
+        public string power = "ElectricCharge";
+
+        int ElectricityId;
+
+        double availAmount;
+        double maxPUFlowPerDeltaTime;
+        float totalPowerNeeded = 0f;
+        int totalPowerProduced = 0;
+
+        int numControllers = 0;
+        int numBatteries = 0;
+
+        const int delaySecs = 10;
+        int delayTics = (int)(delaySecs / Time.fixedDeltaTime);
+        int timeTics = 0;
+
+        const float inv3600 = 1 / 3600;
+
+        Log Log;
+
+        /// <summary>
+        /// Initialize all values
+        /// Set maxValue for the ProgressBar and FloatRange from part config
+        /// </summary>
+        public void Start()
+        {
+            Log = new Log("ModuleDeployableBattery");
+            Log.Info("Start, kerbalEffectAdjustments: " + kerbalEffectAdjustments);
+
+            ElectricityId = PartResourceLibrary.Instance.GetDefinition(power).id;
+
+            var uiRange = this.Fields["maxPowerUnitsFlow"].uiControlCurrent() as UI_FloatRange;
+            uiRange.maxValue = maxPowerUnitsFlow;
+
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                if (part.Resources.Contains(power))
+                {
+                    var uiProgressBar = this.Fields["currentEC"].uiControlCurrent() as UI_ProgressBar;
+                    uiProgressBar.maxValue = (float)part.Resources[power].maxAmount;
+                }
+                maxPUFlowPerDeltaTime = maxPowerUnitsFlow * Time.fixedDeltaTime;
+
+                GetCargoInRange(true);
+            }
+        }
+
+        /// <summary>
+        /// Find all Cargo parts within range, used to calculate power needs
+        /// If firstTime is true, then also find the closest EVA'd kerbal and use that kerbal's stats for any adjustments
+        /// The assumption is that the closest kerbal will be the one which dropped the part
+        /// </summary>
+        /// <param name="firstTime"></param>
+        void GetCargoInRange(bool firstTime = false)
+        {
+            numControllers = 0;
+            numBatteries = 0;
+
+            ModuleGroundExpControl closestModuleControl = null;
+            totalPowerNeeded = 0f;
+            totalPowerProduced = 0;
+
+            Vessel nearestVessel = null;
+            float nearestVesselDistance = -1;
+
+            foreach (var v in FlightGlobals.Vessels)
+            {
+                var moduleControl = v.FindPartModuleImplementing<ModuleGroundExpControl>();
+                if (moduleControl != null)
+                {
+                    float num = Vector3.Distance(base.transform.position, moduleControl.part.transform.position);
+                    if (num <= moduleControl.controlUnitRange)
+                    {
+                        if (closestModuleControl == null)
+                            closestModuleControl = moduleControl;
+                        else
+                        {
+                            float num2 = Vector3.Distance(base.transform.position, closestModuleControl.part.transform.position);
+                            if (num2 < num)
+                                closestModuleControl = moduleControl;
+                        }
+                        numControllers++;
+  
+                        Log.Info("Controller found, power needed: " + moduleControl.powerNeeded + ", experimentsConnected: " + moduleControl.experimentsConnected);
+
+                        int i = moduleControl.powerNeeded.IndexOf(' ');
+
+                        if (i > 0)
+                        {
+                            totalPowerNeeded += float.Parse(moduleControl.powerNeeded.Substring(0, moduleControl.powerNeeded.IndexOf(' ')));
+                        }
+                        else
+                            if (moduleControl.powerNeeded != null && moduleControl.powerNeeded != "")
+                            totalPowerNeeded += float.Parse(moduleControl.powerNeeded);
+                    }
+                }
+                if (v.isEVA)
+                {
+
+                    if (nearestVessel == null)
+                    {
+                        nearestVessel = v;
+                        nearestVesselDistance = Vector3.Distance(base.transform.position, v.Parts[0].transform.position);
+                    }
+                    else
+                    {
+                        float num2 = Vector3.Distance(base.transform.position, v.Parts[0].transform.position);
+                        if (num2 < nearestVesselDistance)
+                        {
+                            nearestVessel = v;
+                            nearestVesselDistance = num2;
+                        }
+                    }
+                }
+
+            }
+            if (closestModuleControl != null)
+            {
+                foreach (var v in FlightGlobals.Vessels)
+                {
+                    var moduleSolar = v.FindPartModuleImplementing<ModuleGroundSciencePart>();
+                    if (moduleSolar != null && moduleSolar.IsSolarPanel)
+                    {
+                        float num = Vector3.Distance(base.transform.position, moduleSolar.part.transform.position);
+                        if (num <= closestModuleControl.controlUnitRange)
+                        {
+                            Log.Info("Solar found, PowerUnitsRequired: " + moduleSolar.PowerUnitsRequired + ", PowerUnitsProduced: " + moduleSolar.PowerUnitsProduced);
+
+                            if (moduleSolar.Enabled)
+                                totalPowerProduced += moduleSolar.PowerUnitsProduced;
+                        }
+                    }
+                    var moduleBattery = v.FindPartModuleImplementing<ModuleDeployableBattery>();
+                    if (moduleBattery != null)
+                    {
+                        float num = Vector3.Distance(base.transform.position, moduleBattery.part.transform.position);
+                        if (num <= closestModuleControl.controlUnitRange)
+                        {
+                            numBatteries++;
+                        }
+                    }
+                }
+            }
+
+            // There is no easy way to know which EVA'd kerbal dropped/placed this, so when created,
+            // as part of the look of all vessels, find the kerbal nearest this part, and use that 
+            // kerbal's skill to get the effect
+
+            if (firstTime)
+            {
+                if (nearestVessel != null && nearestVessel.isEVA && nearestVesselDistance < 2)
+                {
+
+                    Log.Info("FirstTime, nearestVessel: " + nearestVessel.vesselName);
+                    if (nearestVessel.parts[0].protoModuleCrew[0].HasEffect<DeployedSciencePowerSkill>())
+                    {
+                        DeployedSciencePowerSkill effect = nearestVessel.parts[0].protoModuleCrew[0].GetEffect<DeployedSciencePowerSkill>();
+                        if (effect != null)
+                        {
+                            kerbalEffectAdjustments = effect.GetValue();
+                            Log.Info("After effect, kerbalEffectAdjustments: " + kerbalEffectAdjustments + ", Description: " + Localizer.Format("#autoLOC_8002229", effect.GetValue()));
+                            Log.Info("MaxValue: " + ((solarPanelChargeRate / kerbalEffectAdjustments) * part.Resources[power].amount).ToString("F2"));
+                        }
+                    }
+                }
+            }
+            Log.Info("totalPowerNeeded: " + totalPowerNeeded + ", totalPowerProduced: " + totalPowerProduced);            
+        }
+
+        /// <summary>
+        /// Process
+        /// </summary>
+        public void FixedUpdate()
+        {
+            if (timeTics++ > delayTics)
+                GetCargoInRange();
+
+            availAmount = part.Resources[power].amount;
+            if (Enabled && numBatteries > 0)
+            {
+                float powerFlow = (solarPanelChargeRate / kerbalEffectAdjustments) * (totalPowerNeeded - totalPowerProduced) * Time.fixedDeltaTime / numBatteries;
+
+                //Log.Info("totalPowerNeeded: " + totalPowerNeeded + ", totalPowerProduced: " + totalPowerProduced + ", powerFlow: " + powerFlow);
+
+                if (powerFlow > 0)
+                {
+                    if (availAmount >= Math.Min(maxPUFlowPerDeltaTime, powerFlow))
+                    {
+                        actualPowerUnitsProduced =
+                            powerUnitsProduced = (int)maxPowerUnitsFlow;
+                        part.RequestResource(ElectricityId, maxPUFlowPerDeltaTime);
+                    }
+                    else
+                        actualPowerUnitsProduced =
+                            powerUnitsProduced = 0;
+                }
+                else
+                {
+                    actualPowerUnitsProduced =
+                            powerUnitsProduced = 0;
+                    part.Resources[power].amount = Math.Min(part.Resources[power].amount - powerFlow, part.Resources[power].maxAmount);
+                }
+            }
+            currentEC = (float)part.Resources[power].amount;
+            powerUnitHours = currentEC / (solarPanelChargeRate / kerbalEffectAdjustments) * inv3600;
+        }
+
+        public string GetModuleTitle()
+        {
+            return "ModuleDeployableBattery";
+        }
+
+        public override string GetInfo()
+        {
+            string str = power + ": " + part.Resources[power].amount + "/" + part.Resources[power].maxAmount;
+            str += "\nPower-Unit-Hours: " + ((solarPanelChargeRate / kerbalEffectAdjustments) * part.Resources[power].amount).ToString("F2");
+            return str;
+        }
+
+        public string GetPrimaryField()
+        {
+            return power;
+        }
+
+        public Callback<Rect> GetDrawModulePanelCallback()
+        {
+            return null;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,62 @@
+
+Intro
+
+First draft 
+=================================================================
+Gene walked into the conference room, surveyed the faces and sat down heavily.
+He sat for a few seconds, then asked “What’s wrong now?”
+Jebediah jumped up.  “Nothing from a piloting point of view.  We landed the science package on Minmus, set it all up, and then returned”
+Bill’s turn: “The installation and deployment was nominal, everything was working when we left”.
+Gene turned his eyes toward Bob, raised an eyebrow, and just waited
+Bob just rolled his eyes.  “Someone forgot that Minmus doesn’t have sun all the time.  The package lost all power when it rotated into night”
+Bill started whistling tunelessly.
+Bob continued, “and then that same ‘someone’ decided to use different levels of voltage to power everything.  So we can’t even use the batteries available to provide power”
+Bill suddenly became very interested in a speck of dirt on the ceiling.
+Gene got very red in the face, looked at Bill and simply said, “Well?”
+Bill started, and said “Are you talking to me?  Sorry, I wasn’t listening, that spider on the ceiling is making a fascinating web”
+If anything, Gene got even redder, took out a manual and threw it with perfect aim at the ceiling.  The SPLAT was very loud in the suddenly very quiet conference room.
+“I don’t care how you do it, but the next science package we put out there will have power throughout the night.  Do you understand?”
+Bill spluttered “But, the research division has been talking about something called an RTG, claiming it will be available in a few years.  Can’t we just wa..”.
+He stopped, looked at Gene’s face, gulped a bit, and said, “I suppose I can make an adapter of some sort.  I’ll have it ready by the next mission’s launch”.
+.
+..
+...
+....
+A few days later….
+.
+..
+...
+....
+Everyone was again gathered in the conference room.  This time Gene was just sitting with a quizzical expression.  His eyes weren’t rolling.  Yet.
+Bill bustled in, pulling a large package behind him.
+“It’s ready.  Simply deploy this like everything else.  It will provide power and will recharge if there is enough left over from the solar panels.  It’s the new Deployable Battery, and as a bonus, it will work in a ship as well, if needed.  Well, actually, it really is a regular battery, but I’ve added some extra components to convert it to the new power system.”
+
+=============================
+Final Draft
+
+There were cheers and pats on the back as the team stode into the briefing room for the post-flight debriefing... It had been a spectacular mission... flawless... perfect.
+The team set down in the massive Eastern Crater, set up a new Deployed science camp, and returned without incident...
+It was perfect... they thought...
+So why, then, did Gene look like his head was about to explode???
+"Sit down..."
+"What's wrong?" Val whispered nervously...
+"What's wrong??? Gene whispered back, then slammed his notes onto the conference table, sending them flying in every direction... "WHAT'S WRONG???"
+"I... I don't understand," Bill began...
+"Everything was running perfectly," Bob finished
+"Yes... yes... everything was running perfectly..." Wernher von Kerman sighed. "Until the sun set and we lost the signal"
+"But... but we set it up on the light side on the Mun," Jeb argued. "Not the dark side..."
+THUMP.... von Kerman's head hit the table... repeatedly...
+"The... thump... Sun... thump... Still... thump... sets... thump... on the light side... THUMP!!!
+For a few minutes no-one said a word... until Bob whispered almost imperceptibly... "It was the batteries..."
+"What batteries???" Gene sighed...
+"Yeah, that's the point... sir..." Bill grumbled. "There were none."
+"WHAT???" Gege suddenly howled... "What do you mean... no batteries???
+No-one said a word...
+"Werhner..." Gene growled...
+"It... there... uhhhh.... there was a problem at the last second, and ... well,... uhhh.... we had to switch to a different vendor... sir. " von Kerman stammered... "Zaltonic promised they would be compatable..."
+"Zaltonic..." Gene's jaw dropped... "You... you trusted them???  Great... just great!!!"
+"I... I can fix this," Wernher started drawing notes on the table... another habit Gene hated. "Yes... yes... I will contact a friend at Ionic Symphonic... She'll should be able to get me the proper materials..."
+Werhner stopped and grinned at Gene...
+"We can make our own deployable battery packs."
+Gene just sighed... How many times was thing going to happen to him??? He was sure this wasn't the last...
+"You have 48 hours..."
